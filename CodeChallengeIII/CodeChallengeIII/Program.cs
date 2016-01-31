56762f6484b0f2ce46943cfd02e32db962b4cae4
Jaysom <mfloritvidal@gmail.com>
2016-01-31 21:29:14 +0100

    Starting Code Challenge
    First step

---
+++ CodeChallengeIII/CodeChallengeIII/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallengeIII
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            char option = Console.ReadKey().KeyChar;
+            ReadOption(option);
+            Console.ReadLine();            
+        }
+
+        private static void ReadOption(char option)
+        {
+            switch (option)
+            {
+                case 'R':
+                    break;
+                case 'T':
+                    break;
+                case 'B':
+                    break;
+                case 'F':
+                    break;
+                default:
+                    Console.WriteLine("Wrong Operation, try again.");
+                    break;
+            }
+        }
+    }
+}
