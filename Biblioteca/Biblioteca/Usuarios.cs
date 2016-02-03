035a8ecfc5164945568082e38eb2c1b3733eccba
Marçal Montserrat <marcal.montserrat-cantos@sogeti.com>
2016-02-03 23:46:59 +0100

    more logic

--- Biblioteca/Biblioteca/Usuarios.cs
+++ Biblioteca/Biblioteca/Usuarios.cs
@@ -18,6 +18,7 @@ namespace Biblioteca
         public Usuarios()
         {
             this.Prestamos = new HashSet<Prestamos>();
+            this.Multas = new HashSet<Multas>();
         }
     
         public int Id { get; set; }
@@ -25,5 +26,7 @@ namespace Biblioteca
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Prestamos> Prestamos { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Multas> Multas { get; set; }
     }
 }
